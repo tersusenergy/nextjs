47019ae31c8915ff049f7d74866a003a970f9e17
leroylobbo <leroylobo@live.com>
2020-05-03 13:42:20 +0530

    Card

--- src/Grid.js
+++ src/Grid.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Card from './Card';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,7 +22,7 @@ export default function FullWidthGrid() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>Welcome to Tersus Energy</Paper>
+          <Paper className={classes.paper}><Card /></Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
           <Paper className={classes.paper}>Renewable Energy</Paper>
